7ba68c7c861b5f4c6258801849a27536a668608f
tridung <tridung141196@gmail.com>
2017-11-30 22:41:24 +0700

    websocket control led

---
+++ web_server_control_led/web_server_control_led.ino
@@ -0,0 +1,70 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+const char* ssid = "iotmaker_nho";
+const char* password = "@iotmaker.vn";
+const int led = 16; //LED pin = gpio16
+/* HTML sẽ được gởi xuống client */
+const char *html = \
+"<html>\
+  <head>\ 
+    <title>ESP8266 Webserver</title>\
+  </head>\
+  <body>\
+<a href=\"/on\">ON</a>\
+<a href=\"/off\">OFF</a>\
+  </body>\
+</html>";
+/* Web Server lắng nghe ở port 80 */
+/* hàm này được gọi khi trình duyệt truy vấn đến '/on'
+ * sẽ bật đèn LED (0 = on), sau đó chuyển hướng trình duyệt
+ * về lại trang chủ '/'
+ */
+ESP8266WebServer server(80);
+void handleOn() {
+  digitalWrite(led, 0);
+  //server.sendHeader("Location","/");
+  server.send(301);
+  Serial.println("on");
+}
+/* hàm này được gọi khi trình duyệt truy vấn đến '/off'
+ * sẽ tắt đèn LED (1 = off), sau đó chuyển hướng trình duyệt
+ * về lại trang chủ '/'
+ */
+void handleOff() {
+  digitalWrite(led,1);
+  //server.sendHeader("Location","/");
+  server.send(301);
+  Serial.println("off");
+}
+/* hàm này được gọi khi trình duyệt truy vấn đến trang chủ '/'
+ * sẽ gởi dữ liệu HTML, cung cấp các thông tin để bật, tắt LED
+ */
+void handleRoot() {
+  server.send(200, "text/plain", "dung");
+}
+void setup(void){
+  pinMode(led, OUTPUT);
+  digitalWrite(led, 0);
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  /* Chúng ta có thể biết IP của ESP8266
+  * để kết nối tới nhờ gọi hàm này
+  */
+  Serial.println(WiFi.localIP());
+  server.on("/", handleRoot);
+  server.on("/inline", handleOn);
+  server.on("/outline", handleOff);
+  server.begin();
+  Serial.println("HTTP server started");
+}
+void loop(void){
+  server.handleClient();
+}
---
+++ websocket_led_rgb/websocket_led_rgb.ino
@@ -0,0 +1,219 @@
+#include <ESP8266WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <Adafruit_NeoPixel.h>
+#define DATA_PIN 15    //data in pin
+#define TOTAL_LED 12
+//const char *ssid = "dungvo";
+const char* ssid = "Wifi";
+const char* password = "quenpassroi";
+const int LED = 16;
+const int BTN = 0;
+unsigned int setColor;
+int hieuUng=0;
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(TOTAL_LED, DATA_PIN, NEO_GRB + NEO_KHZ800);
+// để đưa đoạn code HTML vào chương trình Arduino, cần chuyển đổi code HTML sang dạng char
+const char index_html[] PROGMEM = ""
+"<!DOCTYPE HTML>"
+"<html>"
+"<head>"
+"<title>ESP8266 WebSocket</title>"
+"</head>"
+"<body>"
+"<div> Webscoket status <span id=\"status\" style=\"font-weight: bold;\"> value=\"disconected\" </span> </div>"
+"<div> ESP8266 Button Status <input type=\"checkbox\" id=\"btn\" name=\"btn\" /> </div>"
+"<div> Control LED <input type=\"checkbox\" id=\"led\" name=\"led\" disabled=\"true\" /> </div>"
+"<div> hieu ung <input type=\"text\" id=\"hu\" name=\"hu\" value=\"1\"></div>"
+"<div> chon mau <input type=\"color\" id=\"color\" name=\"color\" value=\"#ff0000\"> </div>"
+"<script type=\"text/javascript\">"
+"var button = document.getElementById('btn');"
+"var led = document.getElementById('led');"
+"var hieuung = document.getElementById('hu');"
+"var status = document.getElementById('status');"
+"var mau = document.getElementById('color');"
+"var click = document.getElementById('ok');"
+"var url = window.location.host;"
+"var ws = new WebSocket('ws://' + url + '/ws');"
+"ws.onopen = function()"
+"{"
+"status.value = 'Connected';"
+"led.disabled = false;"
+"};"
+"ws.onmessage = function(evt)"
+"{"
+"if(evt.data == 'BTN_PRESSED') {"
+"button.checked = true;"
+"} else if(evt.data == 'BTN_RELEASE') {"
+"button.checked = false;"
+"}"
+"};"
+"ws.onclose = function() {"
+"led.disabled = true;"
+"status.value = 'Disconnected';"
+"};"
+"led.onchange = function() {"
+"var status = 'LED_OFF';"
+"status = hieuung.value;"
+"ws.send(status);"
+"}"
+//"click.click = function() {"
+//"var status2 = 'LED_OFF';"
+//"status2 = hieuung.value;"
+//"ws.send(status2);"
+//"}"
+"</script>"
+"<div>  <input type=\"button\" id=\"ok\" onclick=\"ws.send(hieuung.value+mau.value)\" value=\"ok\"> </div>"
+"</body>"
+"</html>";
+
+AsyncWebServer server(8000);
+AsyncWebSocket ws("/ws");
+
+
+// Hàm xử lí sự kiện trên Server khi client là browser phát sự kiện
+int conver(uint8_t num){
+  int data = (int)num;
+  int out;
+  Serial.println(data);
+  if( data>47 && data <58)
+    out = data -48;
+    else out=data-87;
+    return out;
+}
+void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data,size_t len) {
+  if (type == WS_EVT_DATA && len > 0) { // type: loại sự kiện mà server nhận được. Nếu sự kiện nhận được là từ websocket thì bắt đầu xử lí
+    data[len] = 0;
+  hieuUng = conver(data[0]);
+  setColor = strip.Color(conver(data[2])*16+conver(data[3]), conver(data[4])*16+conver(data[5]), conver(data[6])*16+conver(data[7]));
+  }
+}
+
+void colorWipe(unsigned int c, unsigned int wait)
+{
+  for (int i = TOTAL_LED; i >= 0 ; i--) //numPixels = numLEDs
+  {
+    strip.setPixelColor(i, c); //set color for once LED
+    strip.show();
+  }
+}
+
+uint32_t Wheel(byte WheelPos)
+{
+  if (WheelPos < 85)
+  {
+    return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  }
+  else if (WheelPos < 170)
+  {
+    WheelPos -= 85;
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  else
+  {
+    WheelPos -= 170;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+}
+
+
+//rainbow led for all
+void rainbow(unsigned int wait)
+{
+  int i, j;
+
+  for (j = 0; j < 256; j++)
+  {
+    for (i = 0; i < TOTAL_LED; i++)
+    {
+      strip.setPixelColor(i, Wheel((i + j) & 255));
+    }
+   strip.show();
+  }
+}
+
+// rainbow circle led
+void rainbowCycle(unsigned int wait)
+{
+  uint16_t i, j;
+
+  for (j = 0; j < 256 * 5; j++)
+  { // 5 cycles of all colors on wheel
+    for (i = 0; i < TOTAL_LED; i++)
+    {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+  }
+}
+void hieu_ung1(unsigned int c1)
+{
+  for(int i=1;i<13;i++)
+  {
+        for(int j=1;j<13;j++)
+        {
+          if(j==i){
+            strip.setPixelColor(j,strip.Color(100, 100, 20));
+  //          strip.setPixelColor(j+1,strip.Color(100, 100, 20));
+          }
+
+          else
+            strip.setPixelColor(j, c1);
+        }
+        strip.show();
+        delay(100);
+  }
+}
+
+//change LED from green -> red -> blue -> green (full color)
+
+void setup()
+{
+  pinMode(LED, OUTPUT);
+  pinMode(BTN, INPUT);
+  strip.begin();
+  strip.setBrightness(10);   //setup brightness
+  strip.show();
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  WiFi.mode(WIFI_AP_STA);
+  WiFi.begin(ssid, password);
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+  Serial.printf("STA: Failed!\n");
+  WiFi.disconnect(false);
+  delay(1000);
+  WiFi.begin(ssid, password);
+  }
+//  WiFi.softAP(ssid);
+//
+//  IPAddress myIP = WiFi.softAPIP();
+//  Serial.print("AP IP address: ");
+//  Serial.println(myIP);
+  ws.onEvent(onWsEvent); // gọi hàm onWsEvent
+  server.addHandler(&ws);
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
+  request->send_P(200, "text/html", index_html); // trả về file index.html trên giao diện browser khi browser truy cập vào IP của server
+  });
+  server.begin(); // khởi động server
+}
+
+void loop()
+{
+  if(hieuUng == 1)
+  {
+      colorWipe(setColor, 50);
+  }
+  else if(hieuUng == 2)
+  {
+      hieu_ung1(setColor);
+  }
+  else if(hieuUng == 3)
+  {
+      rainbow(1);
+  }
+  else if(hieuUng == 4)
+  {
+      rainbowCycle(1);
+  }
+}
+
+
+
